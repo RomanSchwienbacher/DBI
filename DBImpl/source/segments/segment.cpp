a31145f842bafefcdbd27f1aa3e8cdd4c2efe821
GrotzkyD <davidgrotzky@gmail.com>
2013-05-23 14:47:16 +0200

    improved Segment::mergeExtents

--- DBImpl/source/segments/segment.cpp
+++ DBImpl/source/segments/segment.cpp
@@ -8,12 +8,12 @@
 #include <algorithm>
 #include "segment.h"
 
-Segment::Segment(uint64_t segId){
+Segment::Segment(uint64_t segId) {
 	Segment::segId = segId;
 	size = 1;
 }
 
-Segment::Segment(std::vector<uint64_t> extents, uint64_t segId){
+Segment::Segment(std::vector<uint64_t> extents, uint64_t segId) {
 
 	Segment::segId = segId;
 	Segment::extents = extents;
@@ -21,25 +21,26 @@ Segment::Segment(std::vector<uint64_t> extents, uint64_t segId){
 	size = calculateSize(extents);
 }
 
-void Segment::grow(std::vector<uint64_t> addExtents){
+void Segment::grow(std::vector<uint64_t> addExtents) {
 
-	extents= mergeExtents(extents, addExtents);
+	extents = mergeExtents(extents, addExtents);
 	size = calculateSize(extents);
 
 }
 
-uint64_t Segment::calculateSize(std::vector<uint64_t> extents){
+uint64_t Segment::calculateSize(std::vector<uint64_t> extents) {
 
 	uint64_t ret = 0;
 
-	for(int i = 0; i < extents.size(); i+=2){
-		ret += (extents.at(i+1) - extents.at(i));
+	for (int i = 0; i < extents.size(); i += 2) {
+		ret += (extents.at(i + 1) - extents.at(i));
 	}
 
 	return ret;
 }
 
-std::vector<uint64_t> Segment::mergeExtents(std::vector<uint64_t> extents1, std::vector<uint64_t> extents2){
+std::vector<uint64_t> Segment::mergeExtents(std::vector<uint64_t> extents1,
+		std::vector<uint64_t> extents2) {
 
 	// append second extent vector to first one
 	extents1.insert(extents1.end(), extents2.begin(), extents2.end());
@@ -47,24 +48,26 @@ std::vector<uint64_t> Segment::mergeExtents(std::vector<uint64_t> extents1, std:
 	// sort entire extent vector
 	std::sort(extents1.begin(), extents1.end());
 
+	// create mergedExtents and fill with first value
+	std::vector<uint64_t> mergedExtents;
+	mergedExtents.push_back(extents1.at(0));
+
 	// merge neighboring extents
-	for(int i = 1; i< extents1.size(); i+=2){
-		if(extents1.at(i) == extents1.at(i+1)){
-			extents1.erase(extents1.begin() + i);
-			extents1.erase(extents1.begin() + i + 1);
+	for (int i = 1; i < extents1.size(); i += 2) {
+		if (extents1.at(i) != extents1.at(i + 1)) {
+			mergedExtents.push_back(extents1.at(i));
 		}
 	}
 
-	return extents1;
+	return mergedExtents;
 }
 
 uint64_t Segment::getSize(){
 
 	return size;
+
 }
 
-std::vector<uint64_t> Segment::getExtents(){
-
+std::vector <uint64_t> Segment::getExtents(){
 	return extents;
-
 }
